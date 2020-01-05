bdaea0f5eee4470810e782fcb507a87953d81607
Vukasin <jovic.vukasin@gmail.com>
2020-01-05 18:19:57 +0100

    init

---
+++ NaucnaCentrala/src/test/java/com/naucnacentrala/NaucnaCentrala/NaucnaCentralaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naucnacentrala.NaucnaCentrala;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaucnaCentralaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
